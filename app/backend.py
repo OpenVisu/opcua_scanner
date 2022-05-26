c6dd8b124d1138f77ea4d677db7f232ed70542d7
Robin Jespersen <jespersen@unitedpartners.de>
2022-05-26 16:49:20 +0200

    fixing import

--- app/backend.py
+++ app/backend.py
@@ -14,7 +14,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import requests
-from opcua.ua.uatypes import VariantType
+from asyncua.ua.uatypes import VariantType
 
 
 class Backend:
